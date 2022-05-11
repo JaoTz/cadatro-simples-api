aeb15322ea07f96fde4d3679ef97fee4ad0af5a1
JaoTz <codgomes@gmail.com>
2022-05-11 20:18:09 -0300

    fix: removing non-existent imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsuarioController } from './usuario/usuario.controller';
-import { UserController } from './user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [UsuarioController, UserController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
